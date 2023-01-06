14641880d024730e63cfc1abb1e44fee9722088d
gustonecrush <codeboxezz@gmail.com>
2023-01-06 16:56:05 +0700

    getting started edited

--- app/src/main/java/com/example/movieapp/Activity/GettingStartedActivity.kt
+++ app/src/main/java/com/example/movieapp/Activity/GettingStartedActivity.kt
@@ -1,12 +1,24 @@
 package com.example.movieapp.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.movieapp.MainActivity
 import com.example.movieapp.R
+import kotlinx.android.synthetic.main.activity_getting_started.*
 
 class GettingStartedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_getting_started)
+
+        btnGettingStartedHandler()
     }
+
+    private fun btnGettingStartedHandler() {
+        btn_getting_started.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
 }
